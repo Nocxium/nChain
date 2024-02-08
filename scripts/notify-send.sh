ec99425ac15feea3012712ee6a9c9008c3624c53
nocxium <ozz.gaming@proton.me>
2024-02-08 07:19:52 +0100

    README.org added

---
+++ scripts/notify-send.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Define the directory
+directory="$HOME/.config/nChain/links"
+
+if [ -d "$directory" ]; then
+  for file in "$directory"/*; do
+    if [ "$(basename "$file")" != "default" ] && [ -f "$file" ]; then
+      filename=$(basename "$file")
+      filename="${filename^}"
+      notify-send -u low "Theme: $filename"
+    fi
+  done
+fi
+
---
+++ scripts/settings.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# launcher="rofi -dmenu -i -p 'Select theme:'"
+
+folders_to_skip=(
+  # "folder_one"
+  # "folder_two"
+)
+
+pre_commands=(
+  # These commands are running before the theme change'"
+)
+
+post_commands=(
+  # Optional commands to run after the theme change. Put each command inside "". Here are some examples:
+  # "pkill -f waybar"
+  # "waybar &"
+  # "swww img $HOME/Pictures/Wallpapers/currentWallpaper-1 --transition-type wipe --transition-fps 60"
+  # "$HOME/.config/nChain/scripts/gsettings.sh &"
+  # "killall .dunst-wrapped"
+  # "kill -SIGUSR1 $(pidof kitty)"
+)
+
+# Define optional categories and subcategories
+declare -A categories
+categories=(
+  # ["Flower"]="Autumn Chiaroscuro Winter-green"
+  # ["Experiment"]="Fern Leaf-seasons MoonScape Snowy-Umbrella Trippy-Mountain Yellow-haze"
+)
